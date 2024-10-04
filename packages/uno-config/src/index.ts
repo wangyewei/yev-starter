03da3bc1261a57aef84aeda78841b916a613da67
王野未 <826036140@qq.com>
2024-10-05 00:47:09 +0800

    feat(uno-config): uno-config setup

---
+++ packages/uno-config/src/index.ts
@@ -0,0 +1,18 @@
+import { defineConfig, presetAttributify, presetUno } from 'unocss'
+import presetRemToPx from '@unocss/preset-rem-to-px'
+
+export default defineConfig({
+  presets: [
+    presetUno,
+    presetAttributify,
+    presetRemToPx({
+      baseFontSize: 4
+    })
+  ],
+  shortcuts: [
+    [
+      'btn',
+      'px-6 py-3 rounded-3 border-none inline-block bg-green-400 text-white cursor-pointer !outline-none hover:bg-green-600 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'
+    ]
+  ]
+})
