a9f9d53a2c21c93fd8893954d309c11cde30f275
王野未 <826036140@qq.com>
2024-10-05 11:57:35 +0800

    chore(uno-config): presetmini

--- packages/uno-config/src/index.ts
+++ packages/uno-config/src/index.ts
@@ -1,4 +1,4 @@
-import { defineConfig, presetAttributify, presetUno } from 'unocss'
+import { defineConfig, presetAttributify, presetUno, presetMini } from 'unocss'
 import presetRemToPx from '@unocss/preset-rem-to-px'
 
 export default defineConfig({
@@ -7,12 +7,14 @@ export default defineConfig({
     presetAttributify,
     presetRemToPx({
       baseFontSize: 4
-    })
+    }),
+    presetMini
   ],
   shortcuts: [
     [
       'btn',
       'px-6 py-3 rounded-3 border-none inline-block bg-green-400 text-white cursor-pointer !outline-none hover:bg-green-600 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'
-    ]
+    ],
+    ['center', 'flex justify-center items-center']
   ]
 })
